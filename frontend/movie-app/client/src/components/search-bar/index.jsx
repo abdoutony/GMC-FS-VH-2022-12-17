f13c50ebc0a4c15c652204cd24eabe726a0f10f9
Abdelkader Lounis <abdouaallb@gmail.com>
2023-01-14 11:36:25 +0100

    add filter to movie app

--- frontend/movie-app/client/src/components/search-bar/index.jsx
+++ frontend/movie-app/client/src/components/search-bar/index.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
-import "./style.css"
-export default function SearchBar() {
+import React from "react";
+import "./style.css";
+export default function SearchBar({ setSearchText }) {
   return (
-     <input type="text" placeholder="search ..." className="form-control search-input" />
-  )
+    <input
+      type="text"
+      placeholder="search ..."
+      className="form-control search-input"
+      onChange={(e) => setSearchText(e.target.value)}
+    />
+  );
 }
--- frontend/movie-app/client/src/pages/movies.jsx
+++ frontend/movie-app/client/src/pages/movies.jsx
@@ -4,33 +4,41 @@ import SearchBar from "../components/search-bar";
 import MovieList from "../components/movie-list";
 import { moviesData } from "../data";
 export default function MoviesPage() {
+  const [searchText, setSearchText] = useState("");
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setData(moviesData);
-      setLoading(false);
-    }, 3000);
-  }, []);
+    if (searchText === "") {
+      setLoading(true);
+      setTimeout(() => {
+        setData(moviesData);
+        setLoading(false);
+      }, 3000);
+    } else {
+      setLoading(true);
+      setTimeout(() => {
+        const searched_data = moviesData.filter((item) =>
+          item.title.includes(searchText)
+        );
+        setData(searched_data);
+        setLoading(false);
+      }, 3000);
+    }
+  }, [searchText]);
   return (
     <HomeLayout>
       <div className="search-wrapper p-4 mt-5">
         <div className="row">
           <div className="col-md-8 offset-md-2">
-            <SearchBar />
+            <SearchBar setSearchText={setSearchText} />
           </div>
         </div>
       </div>
       <div className="movies-wrapper p-4">
         {loading && <h1 className="text-white">Loading ...</h1>}
-        {data && data.length > 0 && (
-          <MovieList data={data} />
-        )}
-        {data && data.length===0 &&  (
-          <h1 className="text-white">No data</h1>
-        )}
+        {!loading && data && data.length > 0 && <MovieList data={data} />}
+        {data && data.length === 0 && <h1 className="text-white">No data</h1>}
         {error && <h1 className="text-white">error</h1>}
       </div>
     </HomeLayout>
